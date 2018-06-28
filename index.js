c2999422016931e3a033febe8375c98a0e3175bc
prmoreira23 <prm.gredes@gmail.com>
2018-06-28 11:06:14 -0400

    Abraham Lincoln saved.

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// // $('#save_lincoln').click(
+// //   () => saveLincoln(parseInt($('#interval').val())))
+// //
+// // function saveLincoln(interval) {
+// //   if(isNaN(interval)) {
+// //     interval = 10000;
+// //   }
+// //   $('#foreground').fadeOut(interval);
+// // }
+//
+const input = document.getElementById("interval");
+const button = document.getElementById("save_lincoln");
+var animation;
+const foreground = document.getElementById("foreground");
+console.log("opacity", foreground.style.opacity);
+let max;
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
+button.addEventListener("click", function(event){
+  foreground.style.opacity = 1;
+  max = parseInt(input.value) || 10000;
+  max /= 10;
+  if(animation){
+    clearInterval(animation);
   }
-  $('#foreground').fadeOut(interval);
+  animation = setInterval(fadeout(), 1);
+}, false);
+
+function fadeout(){
+
+  let counter = 0;
+
+  return function(){
+    console.log(counter, max);
+    if(counter++ < max){
+      foreground.style.opacity -= (1 / max);
+      console.log(foreground.style.opacity);
+    } else {
+      clearInterval(animation);
+    }
+  }
 }
